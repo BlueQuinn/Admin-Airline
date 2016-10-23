ec7b42681d35191abf531437f79e8ea14af3ab0e
Blue <blubluescarlet@gmail.com>
2016-10-23 23:22:47 +0700

    Update flight info
    - Flight class
    - Flight price

--- public/app/flight.js
+++ public/app/flight.js
@@ -4,44 +4,77 @@
 
 app.controller('FlightController', function ($scope, Flight, Airport) {
 
+    var departureDate;
+    $scope.info = ['Thương gia linh hoạt', 'Thương gia tiêu chuẩn', 'Phổ thông linh hoạt', 'Phổ thông tiêu chuẩn', 'Phổ thông tiết kiệm'];
+    $scope.class = ['C', 'C', 'Y', 'Y', 'Y'];
+    $scope.price = ['F', 'S', 'F', 'S', 'C'];
+
+
     var reload = function () {
+
         Flight.query(function (data) {
             $scope.flights = [];
             data.forEach(function (flight) {
-                var item = {};
-                item.flightId = flight.flightId;
-                item.departure = flight.departure;
-                item.arrival = flight.arrival;
-                item.date = (new Date(parseInt(flight.date))).toDateString();
-                item.time = flight.time;
-
                 flight.info.forEach(function (info) {
-                    item.class = info.class;
-                    item.price = info.price;
+                    var item = {};
+                    item.flightId = flight.flightId;
+                    item.departure = flight.departure;
+                    item.arrival = flight.arrival;
+                    item.date = flight.date;
+                    item.time = flight.time;
                     item.total_seat = info.total_seat;
                     item.cost = info.cost;
 
+                    switch (info.class) {
+                        case 'C':
+                            item.class = 'Thương gia';
+                            break;
+                        case 'Y':
+                            item.class = 'Phổ thông';
+                            break;
+                    }
+
+                    switch (info.price) {
+                        case 'F':
+                            item.price = 'Linh hoạt';
+                            break;
+                        case 'S':
+                            item.price = 'Tiêu chuẩn';
+                            break;
+                        case 'C':
+                            item.price = 'Tiết kiệm';
+                            break;
+                    }
+
                     $scope.flights.push(item);
                 });
+
+                var g =1 ;
             });
         });
 
         $scope.flight = {};
-        $scope.flight.info = {};
+        $scope.flight.info = [];
     };
 
     reload();
 
     $scope.getAirports = function () {
-        $scope.airports = Airport.query(function (data) {
-            var a =data;
+        Airport.query(function (data) {
+            $scope.airports = data;
         });
     };
 
     $scope.getAirports();
 
     $scope.add = function (flight) {
-        flight.date = (new Date()).getTime();
+        flight.date = departureDate;
+        flight.time = departureDate;
+        for (var i = 0; i < $scope.info.length; ++i) {
+            flight.info[i].class = $scope.class[i];
+            flight.info[i].price = $scope.price[i];
+            flight.info[i].available_seat = flight.info[i].total_seat;
+        }
         Flight.save(flight, function () {
             console.log(flight);
             reload();
@@ -81,10 +114,24 @@ app.controller('FlightController', function ($scope, Flight, Airport) {
         });
     };
 
-    $(document).ready(function() {
-        $('select').material_select();
-    });
+    /*$(document).ready(function() {
+     $('select').material_select();
+     });*/
 
+    $('input[id="datetimepicker"]').daterangepicker({
+            timePicker: true,
+            singleDatePicker: true,
+            showDropdowns: true,
+            locale: {
+                format: 'hh:mm DD/MM/YYYY'
+            }
+        },
+        function (start, end, label) {
+            departureDate = (new Date(start)).getTime();
 
+            var a = 1;
+
+            // $scope.returnDate = 0;
+        });
 
 });
